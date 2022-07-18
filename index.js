20a4f815ebc490be809639f81eab1d17b9699454
Marysia Brodzki <mjbrodzki@gmail.com>
2022-07-17 22:42:16 -0700

    start of mj choose your own adventure game, left/right and up/down

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 //Your Code here
+
+let response = window.prompt("Do you turn left or right?")
+
+
+console.log(response)
+
+if(response === "left"){
+    console.log("user submitted left")
+
+let response2 = window.prompt("Do you go up or down?")
+if(repsonse2 === "up") {
+    console.log("user submitted up")
+}else if (response2 === "down") {
+    console.log("user submitted down")
+}
+
+} else if (response === "right"){
+    console.log("user submitted right")
+} else {
+    console.log("follow instructions")
+}
